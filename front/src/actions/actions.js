6275783b2590861d371186bf4017375ed7217b29
herve delmas <hdelmas@gmail.com>
2021-09-04 22:39:52 +0200

    optimisation up image

--- front/src/actions/actions.js
+++ front/src/actions/actions.js
@@ -6,12 +6,17 @@ export default {
     return res.data;
   },
   async property(data) {
-    let res = await axios.post('http://localhost:8000/uploads', data.formData);
-    if (res.status == 200) {
-      data.data.update.thumbnail = "http://localhost:8000/uploads/" + res.data.filename;
-      res = await axios.post('http://localhost:8000/property', data);
+    if (data.formData) {
+      let res = await axios.post('http://localhost:8000/uploads', data.formData);
+      if (res.status == 200) {
+        data.data.update.thumbnail = "http://localhost:8000/uploads/" + res.data.filename;
+        res = await axios.post('http://localhost:8000/property', data);
+        return res.data;
+      } else { alert("Upload fail"); }
+    } else {
+      let res = await axios.post('http://localhost:8000/property', data);
       return res.data;
-    } else { alert("Upload fail"); }
+    }
   },
   async deleteProperty(query) {
     let res = await axios.get("http://localhost:8000/deleteProperty?queries=" + query);
