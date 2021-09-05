1c35f67702f6818484f8511f161e26d13ddb1c4b
herve delmas <hdelmas@gmail.com>
2021-09-05 12:39:16 +0200

    msg

--- front/src/actions/actions.js
+++ front/src/actions/actions.js
@@ -10,7 +10,7 @@ export default {
       let res = await axios.post('http://localhost:8000/uploads', data.formData);
       if (res.status == 200) {
         data.data.update.thumbnail = "http://localhost:8000/uploads/" + res.data.filename;
-      } else { alert("Upload fail"); }
+      } else { alert("Upload image fail"); }
     }
     let res = await axios.post('http://localhost:8000/property', data);
     return res.data;
