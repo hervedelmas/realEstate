25f464752bbcaaa4f7ef3b58eb90a52023c29b8b
herve delmas <hdelmas@gmail.com>
2021-09-05 12:21:30 +0200

    Update actions.js

--- front/src/actions/actions.js
+++ front/src/actions/actions.js
@@ -1,22 +1,19 @@
 import axios from "axios"
 
-export default { 
+export default {
   async properties(queries) {
     let res = await axios.get("http://localhost:8000/properties?queries=" + queries);
     return res.data;
   },
-  async property(data) { 
+  async property(data) {
     if (data.formData) {
       let res = await axios.post('http://localhost:8000/uploads', data.formData);
       if (res.status == 200) {
         data.data.update.thumbnail = "http://localhost:8000/uploads/" + res.data.filename;
-        res = await axios.post('http://localhost:8000/property', data);
-        return res.data;
       } else { alert("Upload fail"); }
-    } else {
-      let res = await axios.post('http://localhost:8000/property', data);
-      return res.data;
     }
+    let res = await axios.post('http://localhost:8000/property', data);
+    return res.data;
   },
   async deleteProperty(query) {
     let res = await axios.get("http://localhost:8000/deleteProperty?queries=" + query);
